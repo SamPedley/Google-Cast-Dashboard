38ae8713e8a5f9148f13bf751bad496ff8b66961
sampedley <sjpeds@gmail.com>
2017-07-09 02:00:34 -0500

    more work on sender/reciever config

--- src/app/app-receiver/app.module.ts
+++ src/app/app-receiver/app.module.ts
@@ -1,7 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { AngularFireModule } from 'angularfire2';
-import { environment } from '../environments/environment';
+import { environment } from '../../environments/environment';
 
 import { AppComponent } from './app.component';
 
